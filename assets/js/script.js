efba7498c9317588d168554c24b9dd4ac0d5bb69
Catherine <catheneghan@gmail.com>
2021-07-30 17:09:29 +0100

    gets data from openweather api and displays todays forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-// Weather icons
+var locationEl = document.getElementById("location");
+var todayEl = $(".today");
 
+// Weather icons
 var sunIconEl = "<i class='fas fa-sun'></i>";
 var cloudIconEl = "<i class='fas fa-cloud'></i>";
 var sunCloudIconEl = "<i class='fas fa-cloud-sun'></i>";
@@ -22,4 +24,41 @@ $(".future").each(function() {
     
     var dayOfWeek = dayjs().add(addDays, 'day').format('dddd');
     $(this).find(".day").text(dayOfWeek);  
+});
+
+var loadWeather = function (dailyWeather) {
+    console.log(dailyWeather);
+    // Get weather data
+    var todayTemp = dailyWeather[0].temp.day;
+    var todayWind = dailyWeather[0].wind_speed;
+    var todayHumidity = dailyWeather[0].humidity;
+    var todayUvi = dailyWeather[0].uvi;
+    var weatherCondition = dailyWeather[0].weather[0].id;
+
+    // Display data
+    todayEl.find(".temp").text(todayTemp);
+    todayEl.find(".wind").text(todayWind);
+    todayEl.find(".humid").text(todayHumidity);
+    todayEl.find(".uvi").text(todayUvi);
+
+
+
+}
+
+//Event listener for form submit
+$(".location-search").on("submit", function(event) {
+    event.preventDefault();
+    var locationSearch = locationEl.value.trim();
+    locationEl.value = "";
+    console.log(locationSearch);
+    $(".location").text(locationSearch);
+});
+
+var url = "https://api.openweathermap.org/data/2.5/onecall?lat=53.34&lon=-6.26&exclude=hourly,minutely,alerts&units=imperial&appid=aa5d1cd32b930fb9d0bbbffde6f83c5f";
+
+fetch(url).then(function(response) {
+    response.json().then(function (data) {
+        
+        loadWeather(data.daily);
+    })
 });
