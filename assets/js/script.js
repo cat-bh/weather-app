2aa631256ffc8b8a49c8f3b3db2aae70430d5b0d
Catherine <catheneghan@gmail.com>
2021-08-04 10:30:02 +0100

    displays weather data and assigns icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,9 @@ var cloudIconEl = "<i class='fas fa-cloud'></i>";
 var sunCloudIconEl = "<i class='fas fa-cloud-sun'></i>";
 var rainIconEl = "<i class='fas fa-cloud-rain'></i>";
 var snowIconEl = "<i class='far fa-snowflake'></i>";
-var windIconEl = "<i class='fas fa-wind'></i>";
-
+//var windIconEl = "<i class='fas fa-wind'></i>";
+var fogIconEl = "<i class='fas fa-smog'></i>";
+var thunderIconEl = "<i class='fas fa-bolt'></i>";
 
 // Set date
 var dateEl = document.querySelector(".todays-date");
@@ -26,6 +27,13 @@ $(".future").each(function() {
     $(this).find(".day").text(dayOfWeek);  
 });
 
+var lat = "60.1282";
+var lng = "18.6435";
+
+var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&exclude=hourly,minutely,alerts&units=imperial&appid=aa5d1cd32b930fb9d0bbbffde6f83c5f";
+
+
+// gather data and display to page
 var loadWeather = function (dailyWeather) {
     console.log(dailyWeather);
     // Get weather data
@@ -35,14 +43,51 @@ var loadWeather = function (dailyWeather) {
     var todayUvi = dailyWeather[0].uvi;
     var weatherCondition = dailyWeather[0].weather[0].id;
 
+    var icon = checkIcon(weatherCondition);
+
     // Display data
     todayEl.find(".temp").text(todayTemp);
     todayEl.find(".wind").text(todayWind);
     todayEl.find(".humid").text(todayHumidity);
     todayEl.find(".uvi").text(todayUvi);
+    todayEl.find(".icon").html(icon);
 
+    // Display future data
+    $(".future").each(function() {
+        var i = $(this).attr("data-day");
 
+        weatherCondition = dailyWeather[i].weather[0].id;
+        var icon = checkIcon(weatherCondition);
+        
+        $(this).find(".temp").text(dailyWeather[i].temp.day);
+        $(this).find(".wind").text(dailyWeather[i].wind_speed);
+        $(this).find(".humid").text(dailyWeather[i].humidity);
+        $(this).find(".icon").html(icon);
+    });
+}
 
+function checkIcon (weatherId) {
+    if (weatherId >= 200 && weatherId < 300) {
+        return thunderIconEl;
+    }
+    if ((weatherId >= 300 && weatherId < 510) || (weatherId >= 520 && weatherId < 600)) {
+        return rainIconEl;
+    }
+    if (weatherId === 511 || (weatherId >= 600 && weatherId < 700)) {
+        return snowIconEl;
+    }
+    if (weatherId >= 700 && weatherId < 800) {
+        return fogIconEl;
+    }
+    if (weatherId === 800) {
+        return sunIconEl;
+    }
+    if (weatherId === 801) {
+        return sunCloudIconEl;
+    }
+    if (weatherId >= 802) {
+        return cloudIconEl;
+    }
 }
 
 //Event listener for form submit
@@ -53,8 +98,6 @@ $(".location-search").on("submit", function(event) {
     console.log(locationSearch);
     $(".location").text(locationSearch);
 });
-
-var url = "https://api.openweathermap.org/data/2.5/onecall?lat=53.34&lon=-6.26&exclude=hourly,minutely,alerts&units=imperial&appid=aa5d1cd32b930fb9d0bbbffde6f83c5f";
 
 fetch(url).then(function(response) {
     response.json().then(function (data) {
