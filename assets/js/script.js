221314b6b5c79dd9e2992191739d7f21249a3e2b
Catherine <catheneghan@gmail.com>
2021-08-22 22:01:43 -0700

    Set up local storage to save recent searches, display those searches on screen and load data if clicked. Fixed issue with url getting the wrong coordinates.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,6 @@ var cloudIconEl = "<i class='fas fa-cloud'></i>";
 var sunCloudIconEl = "<i class='fas fa-cloud-sun'></i>";
 var rainIconEl = "<i class='fas fa-cloud-rain'></i>";
 var snowIconEl = "<i class='far fa-snowflake'></i>";
-//var windIconEl = "<i class='fas fa-wind'></i>";
 var fogIconEl = "<i class='fas fa-smog'></i>";
 var thunderIconEl = "<i class='fas fa-bolt'></i>";
 
@@ -27,10 +26,8 @@ $(".future").each(function() {
     $(this).find(".day").text(dayOfWeek);  
 });
 
-var lat = "60.1282";
-var lng = "18.6435";
-
-var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&exclude=hourly,minutely,alerts&units=imperial&appid=aa5d1cd32b930fb9d0bbbffde6f83c5f";
+var lat = "";
+var lng = "";
 
 
 // gather data and display to page
@@ -66,6 +63,7 @@ var loadWeather = function (dailyWeather) {
     });
 }
 
+// Returns the correct icon for the weather conditions
 function checkIcon (weatherId) {
     if (weatherId >= 200 && weatherId < 300) {
         return thunderIconEl;
@@ -90,18 +88,88 @@ function checkIcon (weatherId) {
     }
 }
 
+function getWeather() {
+    var url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lng + "&exclude=hourly,minutely,alerts&units=imperial&appid=aa5d1cd32b930fb9d0bbbffde6f83c5f";
+
+    fetch(url).then(function(response) {
+        response.json().then(function (data) {
+            
+            loadWeather(data.daily);
+        })
+    });
+}
+
+// Get coordinates for city search
+function getCoordinates(city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=aa5d1cd32b930fb9d0bbbffde6f83c5f";
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                
+                $(".location").text(data.name);
+                lat = data.coord.lat;
+                lng = data.coord.lon;
+                
+                getWeather();
+            })
+        } else {
+            // request went wrong
+            $(".location").text("Something went wrong! Please enter just the city name");
+        }
+        
+    })
+}
+
+// Updates the recent search buttons
+function loadRecentSearch() {
+    var searches = localStorage.getItem('recentSearch');
+    if (!searches) {
+        return;
+    }
+    searches = JSON.parse(searches);
+
+    $(".recent-searches").empty();
+
+    for(var i = searches.length -1; i >= 0; i--) {
+        var place = searches[i];
+        var btn = $("<button>")
+        btn.text(place).addClass("btn-recent");
+        $(".recent-searches").append(btn);
+    }
+}
+
+// adds the most recent search to local storage
+function addSearchHistory(location) {
+    var searchArray = localStorage.getItem('recentSearch');
+    if (!searchArray) {
+        searchArray = [];
+    } else {
+        searchArray = JSON.parse(searchArray);
+    }
+    searchArray.push(location);
+
+    if (searchArray.length > 7) {
+        searchArray.splice(0, 1);
+    }
+
+    localStorage.setItem('recentSearch', JSON.stringify(searchArray));
+    loadRecentSearch();
+}
+
+loadRecentSearch();
+
 //Event listener for form submit
 $(".location-search").on("submit", function(event) {
     event.preventDefault();
     var locationSearch = locationEl.value.trim();
     locationEl.value = "";
     console.log(locationSearch);
-    $(".location").text(locationSearch);
+    getCoordinates(locationSearch);
+    addSearchHistory(locationSearch);
 });
 
-fetch(url).then(function(response) {
-    response.json().then(function (data) {
-        
-        loadWeather(data.daily);
-    })
-});
+// Runs a search when recent search buttons are clicked
+$(".recent-searches").on("click", ".btn-recent", function(event) {
+    var location = this.textContent;
+    getCoordinates(location);
+})
